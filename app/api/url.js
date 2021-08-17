1abaf2cd6023a1d99e3fee41d68ea1b0513ae6d0
juhigupta2110 <87846760+juhigupta2110@users.noreply.github.com>
2021-08-17 22:33:30 +0530

    Add files via upload

---
+++ app/api/url.js
@@ -0,0 +1,5 @@
+export const API_END_POINT = 'https://f4fb2833aadb.ngrok.io';
+export const LOGIN_URL = `${API_END_POINT}/DeveloperSignin`;
+export const REGISTER_URL = `${API_END_POINT}/register`;
+export const GET_ALL_RECEIVERS = `${API_END_POINT}/GetAllRecievers`;
+export const ADD_FEADBACK = `${API_END_POINT}/addFeadback`;
